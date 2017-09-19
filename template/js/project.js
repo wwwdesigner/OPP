10791cc08a6f5e3ec0c7e9431ec9666d2d60edf5
wwwdesigner <jakub.matwiejczyk@gmail.com>
2017-09-19 12:22:00 +0200

    Constructor function-car

--- template/js/project.js
+++ template/js/project.js
@@ -1,18 +1,10 @@
-const car = {
-    model: 2,
-    color: 'silver',
-    x: 100,
-    y: 200,
-    doors: 4,
-    tires: 4,
-    maxSpeed: 160,
-    price: 15000,
-    calculatePrice: calculatePrice
-};
 
-function calculatePrice(vat) {
-    return this.price * (vat/100);
-
-    }
-    console.log(car);
-    console.log(car.calculatePrice(23));
+    function Car(brand, model, color, price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.price =price;
+        this.calculatePrice = function (vat) {
+            return price * (1 + (vat / 100));
+        }
+}
